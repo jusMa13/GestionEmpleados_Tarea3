5edfe5c66260e8ffaa40b1eba90e32ae94854836
jusMa13 <jusherramarenco@gmail.com>
2026-04-30 23:49:27 -0600

    Creacion de la parte web, del index

---
+++ GestionEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,81 @@
+﻿using Gestor.BL;
+using Gestor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionEmpleados.Web.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private readonly IEmpleadoRepository _repo;
+
+        public EmpleadosController(IEmpleadoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index(string busqueda = "", int pagina = 1)
+        {
+            int tamanoPagina = 5;
+            var lista = _repo.ObtenerPaginado(pagina, tamanoPagina, busqueda);
+            int totalRegistros = _repo.ContarTotal(busqueda);
+
+            ViewBag.Busqueda = busqueda;
+            ViewBag.PaginaActual = pagina;
+            ViewBag.TotalPaginas = (int)Math.Ceiling((double)totalRegistros / tamanoPagina);
+            ViewBag.TotalRegistros = totalRegistros;
+            return View(lista);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Agregar(empleado);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleado);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var empleado = _repo.ObtenerPorId(id);
+            if(empleado == null)
+            {
+                return NotFound();
+            }
+            return View(empleado);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Actualizar(empleado);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleado);
+        }
+
+        [HttpPost]
+        public IActionResult ToggleActivo(int id)
+        {
+            var empleado = _repo.ObtenerPorId(id);
+            if(empleado != null)
+            {
+                empleado.Activo = !empleado.Activo;
+                _repo.Actualizar(empleado);
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- GestionEmpleados/Program.cs
+++ GestionEmpleados/Program.cs
@@ -1,7 +1,16 @@
+using Gestion.DATA;
+using Gestor.BL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
 
 var app = builder.Build();
 
